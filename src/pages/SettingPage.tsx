7090c90a69d032c6949a203bc7678582b18ce8e6
soohyun4747 <soohyun4747@gmail.com>
2024-11-18 23:28:05 +0900

    setting page with snb

--- src/pages/SettingPage.tsx
+++ src/pages/SettingPage.tsx
@@ -1,9 +1,10 @@
-import { CommonTemplate } from "templates/CommonTemplate";
+import { Snb } from 'components/Snb';
+import { CommonTemplate } from 'templates/CommonTemplate';
 
 export function SettingPage() {
 	return (
 		<CommonTemplate>
-			<></>
+			<Snb selectedIdx={0} />
 		</CommonTemplate>
 	);
 }
--- src/templates/CommonTemplate.tsx
+++ src/templates/CommonTemplate.tsx
@@ -1,13 +1,13 @@
 import { Gnb } from 'components/Gnb';
 import { Snb } from 'components/Snb';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 
 export function CommonTemplate({ children }: { children: ReactNode }) {
+
 	return (
 		<div className='flex'>
 			<Gnb />
 			{children}
-			<Snb selectedIdx={0}/>
 		</div>
 	);
 }
